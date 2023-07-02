eb7d2e158f7423530942c63577ec9e5a96d63991
sumit75757 <patelsumit75757@gmail.com>
2023-07-02 18:21:37 +0530

    googl auth0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   domain_name:"mbeat.us.auth0.com",
-  clint_Id:"4vQW7UYmY3oT0uEx8xNgh9xrhuzZZEUg"
+  clint_Id:"X5RhAXjVxx2Tw8mcefvpmaF59xZBGLRn"
 };
