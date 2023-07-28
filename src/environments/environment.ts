39b7b788d6848e5ca73fcccc8c24834f14c534cd
sumit75757 <patelsumit75757@gmail.com>
2023-07-28 21:03:09 +0530

    pwa add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: true,
   domain_name:"dev-2x5ceivn4roumjbl.us.auth0.com",
   clint_Id:"mMe017OYhbhsgUvJVZ3GWoAuxsSuJzOn",
-  PWA:false
+  PWA:true
 };
 
 /*
